20e7e30887e3717b19a2d67fc329dccf7e68af1a
Shushanskii Konstantin <konstantin.shushanskii@alpari.org>
2018-06-22 23:50:33 +0300

    add eslint

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="container-new">
+          <div className="container-new__item">3 new profile</div>
+          <div className="container-new__button">check now</div>
+          <div className="container-new__close"/>
+        </div>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App"/>
-        );
-    }
-}
+import {App} from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.scss'
+
+ReactDOM.render(<App/>, document.getElementById('root'))
